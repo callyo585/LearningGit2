1b2fa85e0476029ee1c55a4cf13b96657330205d
callyo585 <55347437+callyo585@users.noreply.github.com>
2020-09-08 22:10:07 +0800

    add test2

--- components/layout.js
+++ components/layout.js
@@ -2,6 +2,7 @@ import Navbar from "./navbar";
 import LoginForm from "./login";
 import SignupForm from "./signup";
 import Test from "./test";
+import Test2 from "./test2";
 
 export default ({ children, getPath, firebase }) => (
   <div>
@@ -12,6 +13,7 @@ export default ({ children, getPath, firebase }) => (
     <SignupForm firebase={firebase} />
     <LoginForm firebase={firebase} />
     <Test />
+    <Test2 />
     {children}
     <footer></footer>
   </div>
